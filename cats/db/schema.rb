afc67c42b31ca22f2d40edb006b37b4ea6c398a7
Cassandra <uncertainkitten@gmail.com>
2018-09-12 14:05:53 -0700

    Create user model and tables

--- cats/db/schema.rb
+++ cats/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2017_07_14_042724) do
+ActiveRecord::Schema.define(version: 2018_09_12_210403) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,13 +36,13 @@ ActiveRecord::Schema.define(version: 2017_07_14_042724) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "user_name", null: false
     t.string "password_digest", null: false
     t.string "session_token", null: false
-    t.string "username", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["session_token"], name: "index_users_on_session_token", unique: true
-    t.index ["username"], name: "index_users_on_username", unique: true
+    t.index ["user_name"], name: "index_users_on_user_name", unique: true
   end
 
 end
