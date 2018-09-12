ad4286a1ae61588840a44723199ce54ec390f382
Cassandra <uncertainkitten@gmail.com>
2018-09-12 16:54:54 -0700

    Finish Phase IV

--- cats/app/controllers/sessions_controller.rb
+++ cats/app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+  before_action :login_check, only: [:new]
 
   def new
     @user = User.new
@@ -6,11 +7,11 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+    @user = User.find_by_credentials(params[:user][:user_name], params[:user][:password])
 
     if @user
-      # login!(@user)
-      session[:session_token] = @user.reset_session_token!
+      login!(@user)
+      # session[:session_token] = @user.reset_session_token!
       redirect_to cats_url
     else
       flash.now[:errors] = ['Invalid cat lover >=[']
@@ -19,6 +20,12 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    logout!
+    redirect_to new_session_url
+  end
+
+  def login_check
+    redirect_to cats_url if logged_in?
   end
 
 end
--- cats/app/controllers/users_controller.rb
+++ cats/app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :login_check, only: [:new]
+
   def new
     @user = User.new
     render :new
@@ -6,18 +8,21 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-
     if @user.save
-      # login!(@user)
-      redirect_to user_url(@user)
+      login!(@user)
+      redirect_to cats_url
     else
       flash.now[:errors] = @user.errors.full_messages
       render :new
     end
   end
 
+  def login_check
+    redirect_to cats_url if logged_in?
+  end
+
   private
   def user_params
-    params.require(:users).permit(:user_name, :password)
+    params.require(:user).permit(:user_name, :password)
   end
 end
